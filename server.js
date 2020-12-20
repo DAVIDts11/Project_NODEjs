9168f3a90523f9bb876a1991b1e99313d6e87d22
DAVID <tsiba1@walla.co.il>
2020-12-20 21:49:52 +0200

    begining

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Content-Type', 'application/json');
+    next();
+});
+
+
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something is broken!');
+});
+
+app.listen(port, () => console.log('Express server is running on port ', port));
