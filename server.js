7692def98c3b3ff745b94bd87750e0dca456ec5c
DAVID <tsiba1@walla.co.il>
2020-12-27 21:12:21 +0200

    Access-control

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use("/api/portfolio", PortfolioRouter);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods','POST, PUT, GET, DELETE, OPTIONS');
     res.set('Content-Type', 'application/json');
     next();
 });
