a26440e19475052a1b1133bda85c6706378b9117
DAVID <tsiba1@walla.co.il>
2020-12-27 15:20:10 +0200

    strategy Schema

--- Models/strategy.js
+++ Models/strategy.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const  strategySchema= new Schema({
-    strategy_id: { type: Number},
+    strategy_id: { type: Number, required:true},
     date_beging: { type: Date , default: Date.now},
-    currency: { type: String},
-    amount: { type: Number },
-    take_profit: { type: Number},
-    stop_loss: { type: Number},
-}, {collection: 'schedules'});
+    currency: { type: String, required:true},
+    amount: { type: Number , required:true},
+    take_profit: { type: Number, required:true},
+    stop_loss: { type: Number, required:true},
+}, {collection: 'strategies'});
 
 const Strategy = model('Strategy', strategySchema);
 module.exports = Strategy;
