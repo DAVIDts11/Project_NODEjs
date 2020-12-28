614f97b82d1325477aa29101bf2af5f5dced6b21
Omri93h <Omri93h@gmail.com>
2020-12-28 20:20:06 +0200

    nav fixing, colored symbols

--- Client/js/scripts.js
+++ Client/js/scripts.js
@@ -1,65 +1,83 @@
 $(document).ready(function () {
     insertBalancesValues();
-    setInterval(function () {insertBalancesValues()},30000);
+    setInterval(function () {
+        insertBalancesValues();
+    }, 30000);
     // operationsListeners();
 });
 
+$(document)
+    .ajaxStart(function () {
+        $("#loading").show();
+        $(".balance-usd")
+        .empty()
+        $(".balance-btc")
+        .empty()
+    })
+    .ajaxStop(function () {
+        $("#loading").hide();
+    });
 
 function insertBalancesValues() {
     $.ajax({
-        url: 'http://localhost:8080/api/portfolio',
-        type: 'GET',
+        url: "http://localhost:8080/api/portfolio",
+        type: "GET",
         success: function (result) {
-            
             calculateBtcValue(result);
-           
-            
         },
         error: function () {
             console.log("ERROR !");
-        }
+        },
     });
 }
 
 function getAllPrices(callback) {
     return $.ajax({
-        url: 'http://localhost:8080/api/portfolio/prices',
-        type: 'GET',
+        url: "http://localhost:8080/api/portfolio/prices",
+        type: "GET",
     });
 }
 
 async function calculateBtcValue(balance) {
-    
-    let result = 0, price;
+    let result = 0,
+        price;
     const prices = await getAllPrices();
-    
+
     for (coin_name in balance) {
         console.log("checking " + coin_name);
-        if (coin_name === 'BTC')
-            result += balance['BTC'];
+        if (coin_name === "BTC") result += balance["BTC"];
         else {
             for (symbol in prices)
                 if (coin_name + "BTC" === symbol) {
                     console.log("Entering " + symbol);
                     const value = prices[symbol] * balance[coin_name];
 
-                    if (value > 0.01 && coin_name != "MCO") {
-                        console.log('coin: ' + coin_name + ' | value: ' + value);
+                    if (value > 0.01) {
+                        console.log("coin: " + coin_name + " | value: " + value);
                         result += value;
                     }
-
                 }
         }
         console.log(result);
     }
     console.log("total = " + result);
-    $('.balance-btc').text(result.toFixed(8));
-    $('.balance-btc').append('&nbsp;<i class="fab fa-btc"></i>').hide().show('normal');
+
+    $(".balance-btc").text(result.toFixed(8));
+    $(".balance-btc")
+        .append('&nbsp;<i class="fab fa-btc fa-sm" style="color:orange"></i>')
+        .hide()
+        .show("fast");
+
+    const usd_val = (prices['BTCUSDT'] * result);
+    $(".balance-usd")
+    .empty()
+    .append('≈ ' + usd_val.toLocaleString('en-US', {maximumFractionDigits:2}))
+    .append('&nbsp;<i class="fas fa-dollar-sign" style="color:#008800"></i>')
+    .hide()
+    .show("fast");
 }
 
-function Convert(coin_name, value) {
-
-}
+function Convert(coin_name, value) { }
 // function getBalanceValue(coin){
 
 // }
