2e260d6f763cd9c2300ba378fab41192595369ca
DAVID <tsiba1@walla.co.il>
2020-12-31 22:45:45 +0200

    nikuy kod

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ require("./type_strategies");
 
 
 
-//lastest price:
-// let pair  = 'BNBBTC';
-// binance.prices(pair, (error, ticker) => {
-//     console.info("Price of BNB: ", ticker[pair]);
-//   });
 
 
-
-// binance.bookTickers('BNBBTC', (error, ticker) => {
-//     console.info( ticker["bidPrice"]);
-//   });
 
 
 async function getAllBalances() {
@@ -49,19 +40,6 @@ function getRelevantBalance(balances) {
 };
 
 
-// (async () => {
-//     const prices = await getAllPrices();
-//     for (pair in prices) {
-//         if (pair.endsWith('BTC')) {
-//             console.log(pair);
-//         }
-
-//     }
-// })();
-
-
-
-
 //Getting All Balances:
 (async () => {
     const balances = await getAllBalances();
@@ -70,52 +48,9 @@ function getRelevantBalance(balances) {
 })();
 
 
-
-
-
-
-// let type = "STOP_LOSS_LIMIT";
-// let quantity = 0.3;
-// let price = 0.00125;
-// let stopPrice = 0.0012;
-// binance.sell("BNBBTC", quantity, price, {stopPrice: stopPrice, type: type}, (error, response) => {
-//       console.info("SL response", response);
-//       console.info("order id: " + response.orderId);
-//       // Now you can limit sell with a stop loss, etc.
-//     });
-
-
-
-
-
-//binance.sell("IOTABTC", quantity, price); //.then(console.log("kjak1")).catch(console.log("kjak2"));
-//binance.marketSell("IOTABTC", quantity);
-
-// buy (market) order
-// let quantity = 0.3;
-// binance.marketBuy("BNBBTC", quantity, (error, response) => {
-//   console.info("Market Buy response", response);
-//   console.info("order id: " + response.orderId);
-//   // Now you can limit sell with a stop loss, etc.
-// });
-
-
-
-
-// type = "TAKE_PROFIT";
-// price = 0.00001105;
-// stopPrice = 0.00001108;
-// binance.sell("IOTABTC", quantity, price, {stopPrice: stopPrice, type: type});
 //Getting All Open:
 (async () => {
     const openOrders = await getAllOpenOrders();
     console.log(openOrders);
 })();
 
-// // Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
-// binance.candlesticks("ETHBTC", "5m", (error, ticks, symbol) => {
-//     console.info("candlesticks()", ticks);
-//     let last_tick = ticks[ticks.length - 1];
-//     let [time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored] = last_tick;
-//     console.info(symbol+" last close: "+close);
-//   }, {limit: 1, endTime: Date.now()});
