ede96f7e69539b6f30f600729de66faf039af4ee
DAVID <tsiba1@walla.co.il>
2021-02-14 21:06:49 +0200

    h

--- Controllers/orders.ctrl.js
+++ Controllers/orders.ctrl.js
@@ -1,4 +1,3 @@
-// const { binance } = require('../binance_connection');
 //const Strategy = require('../Models/strategy');
 const Order_Strategy = require('../Models/orders_strategies');
 const { binanceConectedList } = require("./binance.ctrl");
@@ -27,8 +26,6 @@ exports.ordersController = {
                     })();
                     res.json(result_list)
                 })();
-
-
             })
             .catch(err => console.log(`Erorr getting the data from db: ${err}`));
     },
