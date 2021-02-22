67b7ea5b96b6a9ebca08a580d069c45eced6e9c5
DAVID <tsiba1@walla.co.il>
2021-02-22 20:57:57 +0200

    deployment10

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -4,22 +4,22 @@ const passport = require('passport');
 
 
 // auth logout
-router.get('/logout', (req, res) => {
+router.get('/logout/', (req, res) => {
     req.logOut();
     req.redirect('/');
 });
 
 // auth with google+
-router.get('/google', passport.authenticate('google', {
+router.get('/google/', passport.authenticate('google', {
     scope: ['profile']
 }));
 
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+router.get('/google/redirect/', passport.authenticate('google'), (req, res) => {
     // res.send(req.user);
-    res.redirect('https://omridavidproject.netlify.app/login');
+    res.redirect('https://omridavidproject.netlify.app/login/');
 });
 
 module.exports = router;
