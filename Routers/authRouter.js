2a631e82f04c3f1cbfc3f4de6681e26832ca8a1d
DAVID <tsiba1@walla.co.il>
2021-02-22 20:55:27 +0200

    deployment9

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -19,7 +19,7 @@ router.get('/google', passport.authenticate('google', {
 // hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
     // res.send(req.user);
-    res.redirect('http://omridavidproject.netlify.app/login');
+    res.redirect('https://omridavidproject.netlify.app/login');
 });
 
 module.exports = router;
